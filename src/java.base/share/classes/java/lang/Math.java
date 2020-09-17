4fa100b1fa2ad5fa7a45c691597c953ba7006186
Jason Tatton (AWS) <jptatton@amazon.com>
2020-09-17 00:19:54 +0000

    JDK-8251525: AARCH64: Faster Math.signum(fp)

--- src/java.base/share/classes/java/lang/Math.java
+++ src/java.base/share/classes/java/lang/Math.java
@@ -1894,6 +1894,7 @@ public final class Math {
      * @author Joseph D. Darcy
      * @since 1.5
      */
+    @HotSpotIntrinsicCandidate
     public static double signum(double d) {
         return (d == 0.0 || Double.isNaN(d))?d:copySign(1.0, d);
     }
@@ -1915,6 +1916,7 @@ public final class Math {
      * @author Joseph D. Darcy
      * @since 1.5
      */
+    @HotSpotIntrinsicCandidate
     public static float signum(float f) {
         return (f == 0.0f || Float.isNaN(f))?f:copySign(1.0f, f);
     }
@@ -2135,6 +2137,7 @@ public final class Math {
      * and the sign of {@code sign}.
      * @since 1.6
      */
+    @HotSpotIntrinsicCandidate
     public static double copySign(double magnitude, double sign) {
         return Double.longBitsToDouble((Double.doubleToRawLongBits(sign) &
                                         (DoubleConsts.SIGN_BIT_MASK)) |
@@ -2158,6 +2161,7 @@ public final class Math {
      * and the sign of {@code sign}.
      * @since 1.6
      */
+    @HotSpotIntrinsicCandidate
     public static float copySign(float magnitude, float sign) {
         return Float.intBitsToFloat((Float.floatToRawIntBits(sign) &
                                      (FloatConsts.SIGN_BIT_MASK)) |
