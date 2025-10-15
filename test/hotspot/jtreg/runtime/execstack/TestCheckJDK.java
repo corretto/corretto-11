a61c8f3d3f6c59c87ec1da8c9972849babad00b2
Roland Mesde <mesde@amazon.com>
2025-10-15 10:30:39 -0700

    Skipping library noexecstack bit set check for "jfrconv" (#401)

--- test/hotspot/jtreg/runtime/execstack/TestCheckJDK.java
+++ test/hotspot/jtreg/runtime/execstack/TestCheckJDK.java
@@ -38,6 +38,7 @@
 import jdk.test.lib.Asserts;
 import sun.hotspot.WhiteBox;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -49,7 +50,8 @@ public class TestCheckJDK {
     static void checkExecStack(Path file) {
         String filename = file.toString();
         Path parent = file.getParent();
-        if ((parent.endsWith("bin") && !filename.endsWith(".diz")) || filename.endsWith(".so")) {
+        if ((parent.endsWith("bin") && !filename.endsWith(".diz") && !filename.endsWith(File.separator + "jfrconv")) 
+                || filename.endsWith(".so")) {
             if (!WB.checkLibSpecifiesNoexecstack(filename)) {
                 System.out.println("Library does not have the noexecstack bit set: " + filename);
                 testPassed = false;
